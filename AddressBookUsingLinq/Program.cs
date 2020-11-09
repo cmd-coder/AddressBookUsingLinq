ca7579fd5fee505ad3820be41a97c45575ae8e43
CMD <gautamkailashsingh@gmail.com>
2020-11-09 15:58:47 +0530

    [Gautam] Added Columns to the data table.

--- AddressBookUsingLinq/Program.cs
+++ AddressBookUsingLinq/Program.cs
@@ -8,8 +8,23 @@ namespace AddressBookUsingLinq
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Address Book Created And Managed Using Linq");
+            //UC1 -- Create Data Table
             DataTable addressBook = new DataTable();
             Console.WriteLine("Address Book Has Been Created");
+
+            //UC2 -- Insert Columns Into The Data Table
+            addressBook.Columns.Add("FirstName");
+            addressBook.Columns.Add("LastName");
+            addressBook.Columns.Add("Address");
+            addressBook.Columns.Add("City");
+            addressBook.Columns.Add("State");
+            addressBook.Columns.Add("Zip", typeof(int));
+            addressBook.Columns.Add("PhoneNumber");
+            addressBook.Columns.Add("Email");
+            UniqueConstraint uniqueFirstName = new UniqueConstraint(new DataColumn[] { 
+            addressBook.Columns["FirstName"], addressBook.Columns["LastName"]});
+            addressBook.Constraints.Add(uniqueFirstName);
+
         }
     }
 }
